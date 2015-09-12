8966cb9728b54ee5bde05aca68f02ce4df2c6036
chou777 <chou19880426@gmail.com>
2015-09-12 23:32:57 +0800

    Add import tsv store menu task

--- apps/Backend/Tasks/MainTask.php
+++ apps/Backend/Tasks/MainTask.php
@@ -15,33 +15,39 @@ final class MainTask extends \Phalcon\CLI\Task {
         echo "This is a CLI default action.\n";
     }
 
-    public function importStoreAction() {
+    public function importStoreAction($tsv_file) {
       var_dump('importStore');
-      $filename = 'store.tsv';
+      $filename = $tsv_file;
       $myfile = fopen(ROOT . DS . 'cache' . DS . 'store' . DS .$filename, "r") or die("Unable to open file!");
-      $all_keys = array('store', 'categories', 'item', 'coldheat', 'sugar', 'ice', 'size', 'extra');
-      $unique_keys = array('store', 'categories', 'coldheat', 'sugar', 'ice', 'extra');
-      $store_lines = array();
+      if (!isset($tsv_file) || !$myfile) {
+        exit;
+      }
+
+      $header_map = array(
+        'store' => '店名',
+        'categories' => '分類',
+        'drinks' => '品名',
+        'coldheats' => '冷或熱',
+        'sugars' => '甜度',
+        'coldheats_levels' => '冰塊或溫熱',
+        'sizes' => '尺寸',
+        'extras' => '加料',
+      );
+      $all_keys = array('store', 'categories', 'drinks', 'coldheats', 'sugars', 'coldheats_levels', 'sizes', 'extras');
+      $unique_keys = array('store', 'categories', 'coldheats', 'sugars', 'coldheats_levels', 'extras');
+      $drinks = array();
 
       $store_arrays = array(
         'store' => array(),
         'categories' => array(),
-        'item' => array(),
-        'coldheat' => array(),
-        'sugar' => array(),
-        'ice' => array(),
-        'size' => array(),
-        'extra' => array(),
+        'drinks' => array(),
+        'coldheats' => array(),
+        'sugars' => array(),
+        'coldheats_levels' => array(),
+        'sizes' => array(),
+        'extras' => array(),
       );
-      // $header_index = array();
 
-      $store = null;
-      $categories = array();
-      $item = array();
-      $coldheat = array();
-      $sugar = array();
-      $ice = array();
-      $size = array();
       $count = 0;
       $index = array();
 
@@ -50,21 +56,15 @@ final class MainTask extends \Phalcon\CLI\Task {
         $line = null;
         $line = $this->tsvToArray($myfile);
         $line = array_map('trim',$line);
-        array_push($store_lines, $line);
         if ($count === 1) {
           $header_results = $line;
-          $header = array(
-            'store' => array_search('店名', $header_results),
-            'categories' => array_search('分類', $header_results),
-            'item' => array_search('品名', $header_results),
-            'coldheat' => array_search('冷或熱', $header_results),
-            'sugar' => array_search('甜度', $header_results),
-            'ice' => array_search('冰塊或溫熱', $header_results),
-            'size' => array_search('尺寸', $header_results),
-            'extra' => array_search('加料', $header_results),
-          );
+          $header = array();
+          foreach ($header_map as $key => $_name) {
+            $header[$key] = array_search($_name, $header_results);
+          }
           $header = array_flip($header);
         }else{
+          array_push($drinks, $line);
           array_walk($line, function($item, $key) use ($header, &$store_arrays ,$unique_keys) {
             $items = explode(',', $item);
             foreach ($items as $_item) {
@@ -77,10 +77,11 @@ final class MainTask extends \Phalcon\CLI\Task {
           });
         }
       }
-
       $store_plugin = new StorePlugin();
-      $store_plugin->generateStore($store_arrays, $store_lines);
+      $store_plugin->generateStore($store_arrays, $drinks, $header);
       fclose($myfile);
+      var_dump('ImportStoreEnd');
+
     }
 
     private function tsvToArray($myfile) {
